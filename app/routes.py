99ddf838cf6649949dcd9ef89a98b7730117888f
Frank Lehner <frank.lehner@online.de>
2020-03-07 19:07:46 +0100

    Display blog posts

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
@@ -15,19 +15,19 @@ def before_request():
 
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=["GET", "POST"])
+@login_required
 def index():
-    posts = [
-        {
-            "author": {"username": "Frank"},
-            "body": "Beautiful day in Rastatt",
-        },
-        {
-            "author": {"username": "Silke"},
-            "body": "Ich han schon wieder in die Bux geschiss",
-        },
-    ]
-    return render_template("index.html", title="Home", posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your Post is now live")
+        return redirect(url_for("index"))
+
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title="Home", form=form, posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
